693e110e126ba1900f9acec659016fd11cc6cbde
evguevarra <evguevarra08@gmail.com>
2021-09-21 15:32:13 +0800

    initial commit

---
+++ note_app/lib/views/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            Icon(Icons.edit, color: Colors.blueGrey.shade600),
+            const SizedBox(
+              width: 7,
+            ),
+            Text(
+              "Note It",
+              style: TextStyle(
+                color: Colors.blueGrey.shade800,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('No Notes!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueGrey.shade600,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
